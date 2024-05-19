8d99f14656886ab28fc1a15647a9fa22c47c8b6e
Sarath2K <sarath2kdop@gmail.com>
2024-05-19 10:47:39 +0530

    Template Integrated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,71 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('layouts.header')
+@section('master-content')
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth">
+                <div class="row flex-grow">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left p-5">
+                            <div class="brand-logo">
+                                <img src="{{asset('../../assets/images/logo.svg')}}">
+                            </div>
+                            <h4>New here?</h4>
+                            <h6 class="font-weight-light">Signing up is easy. It only takes a few steps</h6>
+                            <form class="pt-3" method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg" id="name"
+                                           placeholder="Username" name="name" value="{{ old('name') }}" required
+                                           autofocus autocomplete="name"/>
+                                    <x-input-error :messages="$errors->get('name')" class="mt-2"/>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-lg" id="email"
+                                           placeholder="Email" name="email" value="{{ old('email') }}" required
+                                           autocomplete="username"/>
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2"/>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" class="form-control form-control-lg" id="phone"
+                                           placeholder="Phone" name="phone" value="{{ old('phone') }}" required/>
+                                    <x-input-error :messages="$errors->get('phone')" class="mt-2"/>
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg" id="password"
+                                           placeholder="Password" name="password" required autocomplete="new-password"/>
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2"/>
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg"
+                                           id="password_confirmation" placeholder="Password"
+                                           name="password_confirmation" required autocomplete="new-password"/>
+                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2"/>
+                                </div>
+                                <div class="mb-4">
+                                    <div class="form-check">
+                                        <label class="form-check-label text-muted">
+                                            <input type="checkbox" class="form-check-input"> I agree to all Terms &
+                                            Conditions </label>
+                                    </div>
+                                </div>
+                                <div class="mt-3">
+                                    <button
+                                        class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn"
+                                        type="submit">SIGN UP
+                                    </button>
+                                </div>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                                <div class="text-center mt-4 font-weight-light"> Already have an account? <a
+                                        href="{{ route('login') }}" class="text-primary">Login</a>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+@endsection
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Beauty Salon Management</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}"/>
+
+    <!-- Select 2 Dropdown-->
+    <!-- Include Select2 CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/css/select2.min.css" rel="stylesheet"/>
+
+    <!-- Include jQuery (required for Select2) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Include Select2 JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/js/select2.min.js"></script>
+</head>
+<body>
+@yield('master-content')
+
+<!-- plugins:js -->
+<script src="{{asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{asset('assets/vendors/chart.js/Chart.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.cookie.js')}}" type="text/javascript"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{asset('assets/js/off-canvas.js')}}"></script>
+<script src="{{asset('assets/js/hoverable-collapse.js')}}"></script>
+<script src="{{asset('assets/js/misc.js')}}"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="{{asset('assets/js/dashboard.js')}}"></script>
+<script src="{{asset('assets/js/todolist.js')}}"></script>
+<!-- End custom js for this page -->
+@stack('scripts')
+</body>
+</html>
